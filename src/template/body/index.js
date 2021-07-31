60b6a7b9b569e5711aaa6e7eb0f0904390cb1144
ekocahyanto007@gmail.com <ekocahyanto007@gmail.com>
2021-07-31 12:18:00 +0700

    product API json in body

--- src/template/body/index.js
+++ src/template/body/index.js
@@ -5,7 +5,34 @@ import { Home, About, Login } from "../page";
 class Body extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      productList: [],
+    };
+  }
+
+  componentDidMount() {
+    const urlFetch = fetch(
+      "https://raw.githubusercontent.com/cahya93/JsonAPI/main/inventory.json"
+    );
+    urlFetch
+      .then((res) => {
+        if (res.status === 200) return res.json();
+      })
+      .then((resJson) => {
+        const dataArr = resJson.map((data) => {
+          return {
+            id: data.id,
+            nameProduct: data.nameProduct,
+            hargaBeli: data.hargaBeli,
+            hargaJual: data.hargaJual,
+            qty: data.qty,
+          };
+        });
+        console.log("JSONDATA:", dataArr);
+        this.setState({
+          productList: dataArr,
+        });
+      });
   }
   renderPage = () => {
     const page = this.props.page;
