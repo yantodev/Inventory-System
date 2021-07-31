ab235ad0f1b7c0b12ac82eb66473c3673003f42b
unknown <dimasrosadi9029@gmail.com>
2021-07-31 13:31:35 +0700

    Tabel Product List

--- src/template/page/productList/productList.js
+++ src/template/page/productList/productList.js
@@ -1,72 +1,87 @@
 import React, { Component } from "react";
+import "./productList.css";
 
 class ProductList extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // id: props.selectedUser.id ? props.selectedUser.id : "",
-      // name: props.selectedUser.name ? props.selectedUser.name : "",
-      // address: props.selectedUser.address ? props.selectedUser.address : ""
-    };
+    this.state = {};
   }
 
-  //   onSaveHandler = () => {
-  //     const { id, name, address } = this.state;
-  //     this.props.saveBarang({ id, namaBarang, hargaBarang, jumlahBarang });
-  //   };
-
-  // setValue = e => this.setState({ [e.target.name]: e.target.value })
-
-  // componentWillUnmount() {
-  //     this.props.resetUserEdit()
-  // }
-
   render() {
-    const { id, namaBarang, hargaBarang, quantity } = this.state;
     return (
-      <table>
-        <tbody>
-          <tr>
-            <td>Nama Barang</td>
-            <td>
-              <input type="hidden" value={id} />
-              <input
-                type="text"
-                name="name"
-                value={namaBarang}
-                onChange={this.setValue}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>Harga Barang</td>
-            <td>
-              <input
-                type="text"
-                name="hargaBarang"
-                value={hargaBarang}
-                onChange={this.setValue}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>Quantity</td>
-            <td>
-              <input
-                type="text"
-                name="quantity"
-                value={quantity}
-                onChange={this.setValue}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td colSpan="2" align="center">
-              {/* <button onClick={this.onSaveHandler}>Save</button> */}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <>
+        <div className="pagination">
+          <span>Prev</span>
+          <span>1</span>
+          <span>2</span>
+          <span className="active">3</span>
+          <span>4</span>
+          <span>5</span>
+          <span>Next</span>
+        </div>
+        <button className="buttonAdd" onclick="addRow()">
+          Add New
+        </button>
+        ;
+        <table class="myTable" width="70%">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama Barang</th>
+              <th>Harga Barang</th>
+              <th>Quantity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Beras Rojo Lele</td>
+              <td>Rp.200000</td>
+              <td>2 Karung</td>
+              <td>
+                <button className="button-edit" onClick={this.editData}>
+                  Edit
+                </button>
+
+                <button className="button-delete" onClick={this.deleteData}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Telor Naga</td>
+              <td>Rp.70000</td>
+              <td>isi 8 butir</td>
+              <td>
+                <button className="button-edit" onClick={this.editData}>
+                  Edit
+                </button>
+
+                <button className="button-delete" onClick={this.deleteData}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Gula Aren</td>
+              <td>Rp.200000</td>
+              <td>2 Karung</td>
+              <td>
+                <button className="button-edit" onClick={this.editData}>
+                  Edit
+                </button>
+
+                <button className="button-delete" onClick={this.deleteData}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </>
     );
   }
 }
