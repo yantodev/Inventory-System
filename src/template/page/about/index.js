24a8426f7885d5dde23c96b5ce26ee1f1f73066e
ekocahyanto007@gmail.com <ekocahyanto007@gmail.com>
2021-08-01 04:56:58 +0700

    update about page

--- src/template/page/about/index.js
+++ src/template/page/about/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import "./about.css";
 
 class About extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: this.props.datas,
-    };
+    this.state = {};
   }
   render() {
-    console.log("data about", this.state.data);
-    return <h1>Ini About {this.state.data}</h1>;
+    return (
+      <>
+        <div className="header">
+          <h1>Our Team Member</h1>
+        </div>
+        <div className="member">
+          <div className="image">
+            <img
+              src="https://avatars.githubusercontent.com/u/49233072?v=4"
+              alt="user"
+            />
+          </div>
+          <div className="nama">
+            <h1>Eko Cahyanto</h1>
+          </div>
+          <div className="title">
+            <h2>Project Manager</h2>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
