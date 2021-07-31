5463aa16145b1ad97892877c59f78cfb6e0203ed
sefrinaldi <sefrinaldi.caniago@gmail.com>
2021-08-01 02:57:52 +0700

    view table diskon

--- src/template/page/diskon/index.js
+++ src/template/page/diskon/index.js
@@ -12,8 +12,7 @@ class Diskon extends Component {
             <>
                 <section class="cards-wrapper">
                     <div class="card-grid-space">
-                        <div class="num">01</div> 
-                        <a class="card" href="https://codetheweb.blog/2017/10/06/html-syntax/" style="--bg-img: url(https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&resize_w=1500&url=https://codetheweb.blog/assets/img/posts/html-syntax/cover.jpg)">                       
+                        <div class="num">01</div>                         
                             <div>
                                 <h1>HTML Syntax</h1>
                                 <p>The syntax of a language is how it works. How to actually write it. Learn HTML syntax…</p>
@@ -21,8 +20,7 @@ class Diskon extends Component {
                                 <div class="tags">
                                     <div class="tag">HTML</div>
                                 </div>
-                            </div> 
-                        </a>                       
+                            </div>                                               
                     </div>                    
                 </section>
             </>
