55e775b0df190aa6e72ddb3081b8a08badfb6456
ekocahyanto007@gmail.com <ekocahyanto007@gmail.com>
2021-08-02 00:34:02 +0700

    bug fix

--- src/template/page/diskon/index.js
+++ src/template/page/diskon/index.js
@@ -1,64 +1,64 @@
-import React, { Component } from 'react';
-import './diskon.css'
-
+import React, { Component } from "react";
+import "./diskon.css";
 
 class Diskon extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id : props.diskon.id ? props.diskon.id : "",
-            nameProduct : props.diskon.nameProduct ? props.diskon.nameProduct : "",
-            thumbnailUrl: props.diskon.thumbnailUrl ? props.diskon.thumbnailUrl : "",
-            diskon: props.diskon.diskon ? props.diskon.diskon : 0
-        }
-    }       
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.diskon.id ? props.diskon.id : "",
+      nameProduct: props.diskon.nameProduct ? props.diskon.nameProduct : "",
+      thumbnailUrl: props.diskon.thumbnailUrl ? props.diskon.thumbnailUrl : "",
+      diskon: props.diskon.diskon ? props.diskon.diskon : 0,
+    };
+  }
 
-    diskonForm = e => {
-        e.preventDefault()                
+  diskonForm = (e) => {
+    e.preventDefault();
 
-        const data = {
-            id : this.state.id,
-            nameProduct : this.state.nameProduct,
-            thumbnailUrl: this.state.thumbnailUrl,
-            diskon: this.state.diskon
-        }
+    const data = {
+      id: this.state.id,
+      nameProduct: this.state.nameProduct,
+      thumbnailUrl: this.state.thumbnailUrl,
+      diskon: this.state.diskon,
+    };
 
-        const { updateDiskon } = this.props
+    const { updateDiskon } = this.props;
 
-        updateDiskon(data)  
-              
-    }
+    updateDiskon(data);
+  };
 
-    setValue = data => {
-        this.setState({            
-            [data.target.name]: data.target.value
-        })
-    }
+  setValue = (data) => {
+    this.setState({
+      [data.target.name]: data.target.value,
+    });
+  };
 
-    render() {
-        // const { diskon } = this.props
-        // console.log("data in diskon : ", diskon);
-        console.log("nilai diskon : ", this.state.diskon);
-        return (
-            <>
-                <div className="content">
-                    <form onClick={this.diskonForm}>
-                        <div className="title">{this.state.nameProduct}</div>
-                        <div className="image">
-                            <img
-                                src={this.state.thumbnailUrl}
-                                alt={this.state.nameProduct}
-                            />
-                        </div>
-                        <div>
-                            <input name="diskon" type="text" value={this.state.diskon} onChange={this.setValue}/>
-                        </div>
-                        <button onClick={()=> this.props.redirect("home")}>Edit</button>
-                    </form>
-                </div>
-            </>
-        );
-    }
+  render() {
+    // const { diskon } = this.props
+    // console.log("data in diskon : ", diskon);
+    console.log("nilai diskon : ", this.state.diskon);
+    return (
+      <>
+        <div className="content">
+          <form onClick={this.diskonForm}>
+            <div className="title">{this.state.nameProduct}</div>
+            <div className="image">
+              <img src={this.state.thumbnailUrl} alt={this.state.nameProduct} />
+            </div>
+            <div>
+              <input
+                name="diskon"
+                type="number"
+                value={this.state.diskon}
+                onChange={this.setValue}
+              />
+            </div>
+            <button onClick={() => this.props.redirect("home")}>Edit</button>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Diskon;
