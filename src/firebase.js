43b0e87ce076e1b9236bed00480323b855d6bdce
Cahya93 <ekocahyanto007@gmail.com>
2021-08-06 05:00:58 +0700

    reactjs firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firabase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDDA5cQEIWJ3BaeCmiYpQatN6l-0YmRg3Y",
+  authDomain: "react-database-b9c30.firebaseapp.com",
+  databaseURL: "https://react-database-b9c30-default-rtdb.firebaseio.com",
+  projectId: "react-database-b9c30",
+  storageBucket: "react-database-b9c30.appspot.com",
+  messagingSenderId: "667387506683",
+  appId: "1:667387506683:web:117f6773f4344af9d0255e",
+  measurementId: "G-2YCZ9J4JRH",
+};
+export default firebase.initializeApp(firebaseConfig);
