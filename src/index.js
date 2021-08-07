ae6f621964b3932feca637507f2dd880153df867
Cahya93 <ekocahyanto007@gmail.com>
2021-08-07 23:15:02 +0700

    update data

--- src/index.js
+++ src/index.js
@@ -5,29 +5,17 @@ import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import Firebase, { FirebaseContext } from "./config/firebase";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import { PersistGate } from "redux-persist/lib/integration/react";
-// import thunk from "redux-thunk";
 import AllReducers from "./reducers";
-// const persistConfig = {
-//   key: "root",
-//   storage: storage,
-// };
-// const persistedReducer = persistReducer(persistConfig, AllReducers);
+
 const store = createStore(
   AllReducers,
-  // persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-// const persistor = persistStore(store);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <FirebaseContext.Provider value={new Firebase()}>
-        {/* <PersistGate persistor={persistor}> */}
         <App />
-        {/* </PersistGate> */}
       </FirebaseContext.Provider>
     </Provider>
   </React.StrictMode>,
