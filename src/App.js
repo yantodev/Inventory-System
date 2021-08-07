c2e5edfae8a16de3d673c94812a12a324d158e29
Cahya93 <ekocahyanto007@gmail.com>
2021-08-07 23:25:14 +0700

    bug login

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     super(props);
     this.state = {
       currentPage: "home",
-      loginStatus: true,
+      loginStatus: false,
     };
   }
   changePage = (page) => {
